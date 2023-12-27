bf4b3a232eda084f7e51edbe4c23d5a6b19e8186
Ashutosh <am3718440@gmail.com>
2023-12-27 16:31:03 +0530

    TinySky: Added clean arc. and required dependencies
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiny_sky/presentation/pages/home_scree.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen();
+      home: HomeScreen()
     );
   }
 }
