959b13f9ddda93d399a9db38cddda85e28aa00a3
Ashutosh <am3718440@gmail.com>
2023-12-27 20:41:02 +0530

    TinySky: Added Hourly section and gradient in background
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- lib/presentation/pages/home_scree.dart
+++ lib/presentation/pages/home_scree.dart
@@ -14,6 +14,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  String monthName(int num){
+    List<String> abbreviatedMonths = [
+      'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+      'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
+    ];
+    return abbreviatedMonths[num-1];
+  }
+
   final _weatherData = WeatherData('672dd5784d1ee2fff09d6767c38498c7');
   WeatherModel? _weatherModel;
 
@@ -41,108 +49,197 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff91DEFF),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Icon(Icons.location_on_outlined, size: 27, color: Colors.white,),
-                    SizedBox(width: 5.w,),
-                    Text(_weatherModel?.city??"Loading City...", style: GoogleFonts.roboto(
-                      fontSize: 18.sp,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w400,
-                    ), ),
-                    SizedBox(width: 5.w,),
-                    Icon(Icons.arrow_drop_down_rounded, color: Colors.white,size: 24.h,)
-                  ],
-                ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                // Color(0xff254659),
+                // Colors.white,
+                Color(0xff91DEFF),
+                Color(0xff47BBE1),
+                // Colors.white
               ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                    height: 187.h,
-                    width: 284.w,
-                    child: Lottie.asset('assets/animations/clouds.json')
-                ),
-                Text("${_weatherModel?.temperature.round()}\u00B0C", style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 64.sp
-                ),),
-                Text("Precipitation", style: TextStyle(color: Colors.white, fontSize: 14.sp),),
-                Text("Max: 34 Min: 28", style: TextStyle(color: Colors.white, fontSize: 14.sp),),
-                SizedBox(height: 20.h,),
-                Container(
-                  height: 47.h,
-                  width: MediaQuery.sizeOf(context).width-40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.r),
-                    color: Color(0xff104084).withOpacity(0.3),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
                     children: [
-                      Row(
-                        children: [
-                          Icon(CupertinoIcons.drop_fill, color: Colors.white, size: 18,),
-                          Text("18%", style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16
-                          ),)
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Icon(CupertinoIcons.thermometer, color: Colors.white, size: 18,),
-                          Text("67%", style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16
-                          ),)
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Icon(CupertinoIcons.wind, color: Colors.white, size: 18,),
-                          Text("24 km/h", style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18
-                          ),)
-                        ],
-                      ),
+                      Icon(Icons.location_on_outlined, size: 27, color: Colors.white,),
+                      SizedBox(width: 5.w,),
+                      Text(_weatherModel?.city??"Loading City...", style: GoogleFonts.roboto(
+                        fontSize: 18.sp,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                      ), ),
+                      SizedBox(width: 5.w,),
+                      Icon(Icons.arrow_drop_down_rounded, color: Colors.white,size: 24.h,)
                     ],
                   ),
-                ),
-                SizedBox(height: 20.h,),
-                Container(
-                  height: 217.h,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.r),
-                    color: Color(0xff104084).withOpacity(0.3),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      height: 187.h,
+                      width: 284.w,
+                      child: Lottie.asset('assets/animations/clouds.json')
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
+                  Text("${_weatherModel?.temperature.round()}\u00B0C", style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 64.sp
+                  ),),
+                  Text("Precipitation", style: TextStyle(color: Colors.white, fontSize: 14.sp),),
+                  Text("Max: 34 Min: 28", style: TextStyle(color: Colors.white, fontSize: 14.sp),),
+                  SizedBox(height: 20.h,),
+                  Container(
+                    height: 47.h,
+                    width: MediaQuery.sizeOf(context).width-40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.r),
+                      color: Color(0xff104084).withOpacity(0.3),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Row(
                           children: [
-                            Text("Today", style: TextStyle(color: CupertinoDynamicColor.withBrightness(color: color, darkColor: darkColor)),)
+                            Icon(CupertinoIcons.drop_fill, color: Colors.white, size: 18,),
+                            Text("18%", style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16
+                            ),)
                           ],
-                        )
+                        ),
+                        Row(
+                          children: [
+                            Icon(CupertinoIcons.thermometer, color: Colors.white, size: 18,),
+                            Text("67%", style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16
+                            ),)
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(CupertinoIcons.wind, color: Colors.white, size: 18,),
+                            Text("24 km/h", style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18
+                            ),)
+                          ],
+                        ),
                       ],
                     ),
                   ),
-                )
-              ],
-            ),
-          ],
+                  SizedBox(height: 20.h,),
+                  Container(
+                    height: 217.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.r),
+                      color: Color(0xff104084).withOpacity(0.3),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("Today", style: GoogleFonts.taprom(color: Colors.white, fontSize: 24.sp, fontWeight: FontWeight.w200,),),
+                              Text(monthName(DateTime.now().month)+', '+ DateTime.now().day.toString(), style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.sp
+                              ),)
+                            ],
+                          ),
+                          SizedBox(height: 10.h,),
+                          SingleChildScrollView(
+                            scrollDirection:Axis.horizontal,
+                            child: Row(
+                              children: [
+                                SizedBox(width: 5.w,),
+                                Container(
+                                  height: 140.h,
+                                  width: 70.w,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20.r),
+                                    border: Border.all(color: Colors.lightBlueAccent)
+                                  ),
+                                  child: TextButton(
+                                      onPressed: (){},
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          Text("30\u00B0C", style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18.sp
+                                          ),),
+                                          Container(
+                                              height: 43.h,
+                                              width: 43.w,
+                                              child: Lottie.asset('assets/animations/light_rain.json'),
+                                          ),
+                                          Text("17:00", style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18.sp
+                                          ),),
+                                        ],
+                                      )
+                                  ),
+                                ),
+                                SizedBox(width: 5.w,),
+                                Container(
+                                  height: 140.h,
+                                  width: 70.w,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20.r),
+                                      border: Border.all(color: Colors.lightBlueAccent)
+                                  ),
+                                  child: TextButton(
+                                      onPressed: (){},
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          Text("30\u00B0C", style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18.sp
+                                          ),),
+                                          Container(
+                                            height: 43.h,
+                                            width: 43.w,
+                                            child: Lottie.asset('assets/animations/light_rain.json'),
+                                          ),
+                                          Text("17:00", style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18.sp
+                                          ),),
+                                        ],
+                                      )
+                                  ),
+                                ),
+                                SizedBox(width: 5.w,),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
