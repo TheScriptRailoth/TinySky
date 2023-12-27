583e0123375ba1b1a7581ea8b54bc16238f9ebbd
Ashutosh <am3718440@gmail.com>
2023-12-27 19:15:02 +0530

    TinySky: Added location on home ui
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tiny_sky/presentation/pages/home_scree.dart';
 
 void main() {
@@ -11,12 +12,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: HomeScreen()
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_,child){
+        return MaterialApp(
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: HomeScreen()
+        );
+      },
     );
   }
 }
--- lib/presentation/pages/home_scree.dart
+++ lib/presentation/pages/home_scree.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:tiny_sky/data/datasources/remote/weather_data.dart';
 import 'package:tiny_sky/data/models/weather_model.dart';
 class HomeScreen extends StatefulWidget {
@@ -36,13 +40,36 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(_weatherModel?.city??"Loading City..."),
-          Text("${_weatherModel?.temperature.round()}\u00B0C"),
-        ],
+      backgroundColor: Color(0xff91DEFF),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.location_on_outlined, size: 27, color: Colors.white,),
+                    SizedBox(width: 5.w,),
+                    Text(_weatherModel?.city??"Loading City...", style: GoogleFonts.roboto(
+                      fontSize: 18.sp,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                    ), ),
+                    SizedBox(width: 5.w,),
+                    Icon(Icons.arrow_drop_down_rounded, color: Colors.white,size: 24.h,)
+                  ],
+                ),
+              ],
+            ),
+            Container(child: Lottie.asset('assets/animations/clouds.json')),
+          ],
+        ),
       ),
     );
   }
 }
+// Text(_weatherModel?.city??"Loading City..."),
+// Text("${_weatherModel?.temperature.round()}\u00B0C"),
+// Text(_weatherModel!.condition??"Loading Condition...")
