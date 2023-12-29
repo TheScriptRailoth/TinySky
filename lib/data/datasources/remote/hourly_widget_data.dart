9bc16f117d5505b8fdd595a0af1d4f242cbc018c
Ashutosh <am3718440@gmail.com>
2023-12-30 00:22:30 +0530

    TinySky: Added API Response for Daily Data
    Signed-off-by: Ashutosh <am3718440@gmail.com>

--- lib/data/datasources/remote/hourly_widget_data.dart
+++ lib/data/datasources/remote/hourly_widget_data.dart
@@ -8,7 +8,7 @@ class HourlyWidgetData{
   HourlyWidgetData();
   Future<HourlyWidgetModel>getHourlyData() async{
     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    final response2= await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/onecall?lat=${position.latitude}&lon=${position.longitude}&exclude=daily,minutely,currently,current&appid=105d997a8a1977cb138167503eb7afa1'));
+    final response2= await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/onecall?lat=${position.latitude}&lon=${position.longitude}&exclude=minutely,currently,current&appid=105d997a8a1977cb138167503eb7afa1'));
     if(response2.statusCode==200){
       print("Respose 2: ${response2.body}");
       return HourlyWidgetModel.fromJson(jsonDecode(response2.body));
--- lib/data/models/hourly_widget_model.dart
+++ lib/data/models/hourly_widget_model.dart
@@ -1,19 +1,38 @@
+import 'package:intl/intl.dart';
+
 class HourlyWidgetModel{
   final List<double> temperature;
   final List<String> time;
   final List<String> condition;
 
+  final List<String> dayName;
+  final List<double> dayMinTemp;
+  final List<double> dayMaxTemp;
+  final List<String> dayCondition;
+
   HourlyWidgetModel({
     required this.temperature,
     required this.time,
     required this.condition,
+
+    required this.dayName,
+    required this.dayMinTemp,
+    required this.dayMaxTemp,
+    required this.dayCondition,
   });
 
   factory HourlyWidgetModel.fromJson(Map<String,dynamic> json){
     List<dynamic> listData = json['hourly'];
+    List<dynamic> listData2 =json['daily'];
+
     List<String> timeValue = <String>[];
     List<String> conditionValues= <String>[];
     List<double> tempValues= <double>[];
+
+    List<String> dayNameValues = <String>[];
+    List<String> dayConditionValues =<String>[];
+    List<double> dayMinTempValues = <double>[];
+    List<double> dayMaxTempValues =<double>[];
 
 
     for(int i=0;i<listData.length;i++){
@@ -21,18 +40,34 @@ class HourlyWidgetModel{
       var time=listData[i]['dt'];
       var dateTime = DateTime.fromMillisecondsSinceEpoch(time * 1000, isUtc: true);
       var formattedTime = dateTime.toString();
-
-
       double temp=(listData[i]['temp'].toDouble())-273.15;
       String condition= listData[i]['weather'][0]['main'];
+
+      int timestamp = listData2[i]['dt'];;
+      DateTime dateTime2 = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000, isUtc: true);
+      String dayName = DateFormat.E().format(dateTime2);
+      double minTemp = (listData2[i]['temp']['min'].toDouble())-273.15;
+      double maxTemp = (listData2[i]['temp']['max'].toDouble())-273.15;
+      String dayCon = listData2[i]['weather'][0]['main'];
+
+      dayNameValues.add(dayName);
+      dayConditionValues.add(dayCon);
+      dayMinTempValues.add(minTemp);
+      dayMaxTempValues.add(maxTemp);
+
       conditionValues.add(condition);
       timeValue.add(formattedTime);
       tempValues.add(temp);
     }
+
     return HourlyWidgetModel(
       temperature: tempValues,
       time: timeValue,
       condition: conditionValues,
+      dayName: dayNameValues,
+      dayCondition: dayConditionValues,
+      dayMaxTemp: dayMaxTempValues,
+      dayMinTemp: dayMinTempValues,
     );
   }
 }
